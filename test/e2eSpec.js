6eb2cd92483a07f6d4072d3368bf25d8f2052c1c
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-04-30 21:09:30 +0200

    should not match same direction offers

--- test/e2eSpec.js
+++ test/e2eSpec.js
@@ -3,7 +3,7 @@ var chai = require('chai'),
   matcher = require('../lib/matcher');
 
 describe('TradeMatcherJS', function() {
-  it('should match equal opposite transactions', function(done) {
+  it('should match opposite offers', function(done) {
     var m = new matcher.Matcher();
     m.on('match', function(offer1, offer2) {
       expect(offer1.oid).to.equal(123);
@@ -15,6 +15,15 @@ describe('TradeMatcherJS', function() {
     m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 10, oid: 901 });
 
     expect(m.availableOffers).to.equal(0);
+  })
+
+  it('should not match same direction offers', function() {
+    var m = new matcher.Matcher();
+
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 10, oid: 123 });
+    m.send({ product: 'FLOWERS', price: 1, is: 'sell', amount: 10, oid: 901 });
+
+    expect(m.availableOffers).to.equal(2);
   })
 
   it('should match offers of same product', function() {
