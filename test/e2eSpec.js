4fdaa829ec93d0691f5d28e84ea5676c6b54f78c
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-05-01 10:25:38 +0200

    matching one offer to many

--- test/e2eSpec.js
+++ test/e2eSpec.js
@@ -3,35 +3,6 @@ var chai = require('chai'),
   matcher = require('../lib/matcher');
 
 describe('TradeMatcherJS', function() {
-  it('should match opposite offers', function(done) {
-    var m = new matcher.Matcher();
-    m.on('match', function(offer1, offer2) {
-      expect(offer1.oid).to.equal(123);
-      expect(offer2.oid).to.equal(901);
-      done();
-    });
-
-    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 10, oid: 123 });
-    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 10, oid: 901 });
-
-    expect(m.availableOffers).to.equal(0);
-  })
-
-  it('should match up to amount', function(done) {
-    var m = new matcher.Matcher();
-    m.on('match', function(offer1, offer2) {
-      expect(offer1.oid).to.equal(123);
-      expect(offer1.amount).to.equal(5);
-      expect(offer2.amount).to.equal(5);
-      expect(offer2.oid).to.equal(901);
-      done();
-    });
-
-    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 5, oid: 123 });
-    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 10, oid: 901 });
-
-    expect(m.availableOffers).to.equal(1);
-  })
 
   it('should not match same direction offers', function() {
     var m = new matcher.Matcher();
@@ -50,4 +21,88 @@ describe('TradeMatcherJS', function() {
 
     expect(m.availableOffers).to.equal(2);
   })
+
+  it('should match offers of same price', function() {
+    var m = new matcher.Matcher();
+
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 10, oid: 123 });
+    m.send({ product: 'COOKIES', price: 1.5, is: 'buy', amount: 10, oid: 901 });
+
+    expect(m.availableOffers).to.equal(2);
+  })
+
+  it('should match opposite offers', function(done) {
+    var m = new matcher.Matcher();
+    m.on('match', function(offer1, offer2) {
+      expect(offer1.oid).to.equal(901);
+      expect(offer2.oid).to.equal(123);
+      done();
+    });
+
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 10, oid: 123 });
+    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 10, oid: 901 });
+
+    expect(m.availableOffers).to.equal(0);
+  })
+
+  it('should match up to amount available on opposite side', function(done) {
+    var m = new matcher.Matcher();
+    m.on('match', function(offer1, offer2) {
+      expect(offer1.amount).to.equal(5);
+      expect(offer1.oid).to.equal(901);
+      expect(offer2.oid).to.equal(123);
+      expect(offer2.amount).to.equal(5);
+      done();
+    });
+
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 10, oid: 123 });
+    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 5, oid: 901 });
+
+    expect(m.availableOffers).to.equal(1);
+  })
+
+  it('should match up to amount available on this side', function(done) {
+    var m = new matcher.Matcher();
+    m.on('match', function(offer1, offer2) {
+      expect(offer1.amount).to.equal(5);
+      expect(offer1.oid).to.equal(901);
+      expect(offer2.oid).to.equal(123);
+      expect(offer2.amount).to.equal(5);
+      done();
+    });
+
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 5, oid: 123 });
+    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 10, oid: 901 });
+
+    expect(m.availableOffers).to.equal(1);
+  })
+
+  it('should match until funds available', function(done) {
+    var m = new matcher.Matcher();
+    var counter = 0;
+    m.on('match', function() {
+      (++counter == 2) && done();
+    });
+
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 10, oid: 123 });
+    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 5, oid: 901 });
+    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 5, oid: 902 });
+
+    expect(m.availableOffers).to.equal(0);
+  })
+
+  it('should match up to amount available on this side', function(done) {
+    var m = new matcher.Matcher();
+    m.on('match', function(offer1, offers2) {
+      expect(offers2.length).to.equal(2);
+      done();
+    });
+
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 5, oid: 123 });
+    m.send({ product: 'COOKIES', price: 1, is: 'sell', amount: 5, oid: 124 });
+    m.send({ product: 'COOKIES', price: 1, is: 'buy', amount: 10, oid: 901 });
+
+    expect(m.availableOffers).to.equal(0);
+  })
+
 });
