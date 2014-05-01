602b87092e99290759b3e0b83b02a4e0cac26085
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-05-01 11:36:51 +0200

    refactor test utils to separate file

--- test/e2eSpec.js
+++ test/e2eSpec.js
@@ -1,5 +1,9 @@
 var chai = require('chai'),
   expect = chai.expect,
+  utils = require('./utils'),
+  deal = utils.deal,
+  offer = utils.offer,
+  expectDeals = utils.expectDeals,
   matcher = require('../lib/matcher');
 
 describe('TradeMatcherJS', function() {
@@ -100,64 +104,4 @@ describe('TradeMatcherJS', function() {
 
     expect(m.availableOffers).to.equal(0);
   })
-
-  it('should produce offer', function() {
-    expect(offer("901: buy 10 COOKIES for 1"))
-    .to.deep.equal({
-      product: 'COOKIES',
-      price: 1,
-      is: 'buy',
-      amount: 10,
-      oid: 901
-    });
-  });
-
-  it('should produce deal', function() {
-    expect(deal("126 buys 10: 2 from 123, 8 from 125"))
-    .to.deep.equal([
-      {oid: 126, amount: 10},
-      [{oid: 123, amount: 2}, {oid: 125, amount: 8}]
-    ]);
-  })
-
 });
-
-function offer(descr) {
-  // "123: sell 1 COOKIES for 3"
-  var args = descr.split(' ');
-  return {
-    is: args[1],
-    amount: parseFloat(args[2]),
-    product: args[3],
-    price: parseFloat(args[5]),
-    oid: parseInt(args[0].replace(":", "")) };
-}
-
-function deal(descr) {
-  // "126 buys 10: 2 from 123, 8 from 125"
-  var deal = descr.split(":");
-  var args1 = deal[0].split(' ');
-  var args2 = deal[1].split(',');
-  var offers = [];
-  for (var i=0; i < args2.length; i++) {
-    var arg = args2[i].trim().split(' ');
-    offers.push({oid: parseInt(arg[2]), amount: parseFloat(arg[0])});
-  }
-  var offer1 = {oid: parseInt(args1[0]), amount: parseFloat(args1[2])};
-  return [offer1, offers.length == 1? offers[0]: offers];
-}
-
-function expectDeals(matcher, expectedMatches, callback) {
-  var actualMatches = [];
-  for (var i=0; i < expectedMatches.length; i++) {
-    expectedMatches[i] = deal(expectedMatches[i]);
-  }
-
-  matcher.on('match', function(offer1, offer2) {
-    actualMatches.push([offer1, offer2]);
-    if (actualMatches.length === expectedMatches.length) {
-      expect(actualMatches).to.deep.equal(expectedMatches);
-      callback();
-    }
-  });
-}
---
+++ test/utils.js
@@ -0,0 +1,70 @@
+var chai = require('chai'),
+  expect = chai.expect;
+
+describe('Test utils', function() {
+
+  it('should produce offer', function() {
+    expect(offer("901: buy 10 COOKIES for 1"))
+    .to.deep.equal({
+      product: 'COOKIES',
+      price: 1,
+      is: 'buy',
+      amount: 10,
+      oid: 901
+    });
+  });
+
+  it('should produce deal', function() {
+    expect(deal("126 buys 10: 2 from 123, 8 from 125"))
+    .to.deep.equal([
+      {oid: 126, amount: 10},
+      [{oid: 123, amount: 2}, {oid: 125, amount: 8}]
+    ]);
+  })
+});
+
+function offer(descr) {
+  // "123: sell 1 COOKIES for 3"
+  var args = descr.split(' ');
+  return {
+    is: args[1],
+    amount: parseFloat(args[2]),
+    product: args[3],
+    price: parseFloat(args[5]),
+    oid: parseInt(args[0].replace(":", "")) };
+}
+
+function deal(descr) {
+  // "126 buys 10: 2 from 123, 8 from 125"
+  var deal = descr.split(":");
+  var args1 = deal[0].split(' ');
+  var args2 = deal[1].split(',');
+  var offers = [];
+  args2.forEach(function(arg2) {
+    var arg = arg2.trim().split(' ');
+    offers.push({oid: parseInt(arg[2]), amount: parseFloat(arg[0])});
+  })
+  var offer1 = {oid: parseInt(args1[0]), amount: parseFloat(args1[2])};
+  return [offer1, offers.length == 1? offers[0]: offers];
+}
+
+function expectDeals(matcher, expectedMatches, callback) {
+  var actualMatches = [];
+  expectedMatches.forEach(function(match, i) {
+    expectedMatches[i] = deal(match);
+  });
+
+  matcher.on('match', function(offer1, offer2) {
+    actualMatches.push([offer1, offer2]);
+    if (actualMatches.length === expectedMatches.length) {
+      expect(actualMatches).to.deep.equal(expectedMatches);
+      callback();
+    }
+  });
+}
+
+module.exports = {
+  offer: offer,
+  deal: deal,
+  expectDeals: expectDeals
+}
