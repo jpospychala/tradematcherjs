cf3bdf22635dc52004a05a5fe04bda9fc2ecf6af
Jacek Pospychala <jacek.pospychala@gmail.com>
2014-05-03 17:38:16 +0200

    should store offers in mongo and restore them from db

--- test/storageSpec.js
+++ test/storageSpec.js
@@ -11,12 +11,16 @@ describe('Store', function() {
   var storage = new Storage();
 
   before(function(done) {
-    storage.open().then(done);
+    storage.open().then(done, done);
   })
 
   after(function(done) {
-    storage.close().then(done);
+    storage.close().then(done, done);
   })
+
+  beforeEach(function(done) {
+    storage.clear().then(function() {done();}, done);
+  });
 
   it('should store arriving offers and deals', function(done) {
     storage.writeOffer(offer('1: sell 1 COOKIES for 3'))
@@ -27,8 +31,30 @@ describe('Store', function() {
       return storage.writeDeal('2 deals 3: 3 from 1');
     })
     .then(function() {
-      expect(storage.load()).to.eventually.deep.equal([]);
-      done();
+      expect(storage.load()).to.eventually.deep.equal({
+        COOKIES: {
+          "3": {
+            sell: [],
+            buy: []
+          }
+        }
+      }).notify(done);
+    }, function(error) {
+      done(error);
+    })
+  })
+
+  it('should store arriving offers', function(done) {
+    storage.writeOffer(offer('1: sell 1 COOKIES for 3'))
+    .then(function() {
+      expect(storage.load()).to.eventually.deep.equal({
+        COOKIES: {
+          "3": {
+            sell: [{oid:1, amount: 1}],
+            buy: []
+          }
+        }
+      }).notify(done);
     }, function(error) {
       done(error);
     })
